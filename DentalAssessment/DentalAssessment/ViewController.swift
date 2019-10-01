5133ab718bb8db2b7deded5094bb6ead16cfd860
deepanshgandhi <34491946+deepanshgandhi@users.noreply.github.com>
2019-10-01 17:05:37 +0530

    Add files via upload
    Basic Workflow

---
+++ DentalAssessment/DentalAssessment/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DentalAssessment
+//
+//  Created by student on 01/10/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+
+
+}
+
