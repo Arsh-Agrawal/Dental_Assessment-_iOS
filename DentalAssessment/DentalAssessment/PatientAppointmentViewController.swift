8cfac26ecec851fe8c49726b70966a6a3d60b248
Arsh-Agrawal <arsh.167.agrawal@gmail.com>
2019-10-12 15:05:52 +0530

    messages show in item(nav bar navigation)-patient

---
+++ DentalAssessment/DentalAssessment/PatientAppointmentViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PatientAppointmentViewController.swift
+//  DentalAssessment
+//
+//  Created by Arsh Agrawal on 12/10/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class PatientAppointmentViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var postData = ["message1","message2","message3"]
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell")
+        cell?.textLabel?.text = postData[indexPath.row]
+        return cell!
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
