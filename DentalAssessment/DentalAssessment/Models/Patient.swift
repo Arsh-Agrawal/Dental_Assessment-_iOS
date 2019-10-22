746d755925754549de52a2e4230285fcea74e70e
Chaitanya <chaitanya2.1999@gmail.com>
2019-10-22 19:44:06 +0530

    Reorganise files and implement DocumentSerializable for Patient

---
+++ DentalAssessment/DentalAssessment/Models/Patient.swift
@@ -0,0 +1,39 @@
+//
+//  Patient.swift
+//  DentalAssessment
+//
+//  Created by student on 08/10/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import Foundation
+
+class Patient: DocumentSerializable {
+    var id: String
+    var name: String
+    var age: Int
+    var sex: Int
+    var phone: String
+    var address: String
+    var maritalStatus: Bool?
+    var occupation: String?
+    init(id: String, name: String, age: Int, sex: Int, phone: String, address: String, maritalStatus: Bool?, occupation: String?) {
+        self.id = id
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.phone = phone
+        self.address = address
+        self.maritalStatus = maritalStatus
+    }
+    required convenience init?(from dict: [String : Any]) {
+        guard let id = dict["id"] as? String else {return nil}
+        guard let name = dict["name"] as? String else {return nil}
+        guard let age = dict["age"] as? Int else {return nil}
+        guard let sex = dict["sex"] as? Int else {return nil}
+        guard let phone = dict["phone"] as? String else {return nil}
+        guard let address = dict["address"] as? String else {return nil}
+        
+        self.init(id: id, name: name, age: age, sex: sex, phone: phone, address: address, maritalStatus: dict["maritalStatus"] as? Bool, occupation: dict["occupation"] as? String)
+    }
+}
