efe5db294348e8514722300261d9264ce08f8386
Chaitanya <chaitanya2.1999@gmail.com>
2019-10-28 17:01:15 +0530

    Implement more fields in patient assessment form
    Implement all text based and selection fields

--- DentalAssessment/DentalAssessment/PatientAssessmentViewController.swift
+++ DentalAssessment/DentalAssessment/PatientAssessmentViewController.swift
@@ -22,10 +22,12 @@ class CaseSheetViewController: FormViewController{
                 row.title = "Age"
                 row.placeholder = "25"
             }
-            <<< ActionSheetRow<String>() {
-                $0.title = "Sex"
-                $0.selectorTitle = "Pick sex"
-                $0.options = ["Male","Female"]
+            <<< SegmentedRow<String>() { row in
+                row.title = "Sex"
+                row.options = ["Male","Female"]
+                }.cellSetup { cell, _ in
+                    guard let titleLabel = cell.titleLabel else {return}
+                    cell.segmentedControl.widthAnchor.constraint(equalTo: titleLabel.widthAnchor).isActive = true
             }
             <<< PhoneRow() { row in
                 row.title = "Phone"
@@ -38,40 +40,209 @@ class CaseSheetViewController: FormViewController{
                 row.title = "Address"
                 row.placeholder = "Street Address\nLocality\nCity, State\nPincode"
             }
-              +++ Section("Assessment")
+            
+            
+            +++ Section("Assessment")
             <<< TextRow() { row in
                 row.title = "Hosptial Number"
                 row.placeholder = "123"
             }
-            <<< DateRow() {
-                $0.value = Date()
-                $0.title = "Date"
+            <<< DateRow() { row in
+                row.value = Date()
+                row.title = "Date"
                 
             }
             <<< LabelRow() { row in
-                row.title = "Illness History"
+                row.title = "Present Illness History"
             }
             <<< TextAreaRow { row in
+                row.placeholder = "Enter history of present illness"
             }
             <<< LabelRow() { row in
                 row.title = "Medical History"
             }
             <<< TextAreaRow { row in
+                row.placeholder = "Enter relevant medical history"
             }
             <<< LabelRow() { row in
                 row.title = "Family History"
             }
             <<< TextAreaRow { row in
+                row.placeholder = "Enter family history"
             }
             <<< LabelRow() { row in
-                row.title = "Personal History"
+                row.title = "Past dental History"
             }
             <<< TextAreaRow { row in
+                row.placeholder = "Enter dental history"
             }
-            <<< LabelRow() { row in
-                row.title = "Illness History"
+            
+            
+            +++ Section("General Examination")
+            <<< TextRow() { row in
+                row.title = "Build and gait"
+                row.placeholder = "description"
             }
-            <<< TextAreaRow { row in
+            <<< TextRow() { row in
+                row.title = "Vital signs"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Other findings"
+                row.placeholder = "description"
+            }
+        
+        
+            +++ Section("Extraoral Examination")
+            <<< TextRow() { row in
+                row.title = "Eyes, ears and nose"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Facial symmetry"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Facial profile"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Lips"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "TMJ Examination"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Lymph nodes"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Mouth opening"
+                row.placeholder = "description"
+            }
+        
+            
+            +++ Section("Intraoral examination")
+            +++ Section("Soft tissue examination")
+            <<< TextRow() { row in
+                row.title = "Labial Mucosa"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Buccal Mucosa"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Palate"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Floor of mouth"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Toungue"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Vestibules"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Faucial Pillars"
+                row.placeholder = "description"
+            }
+            +++ Section("Gingiva")
+            <<< TextRow() { row in
+                row.title = "Color"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Contour"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Position"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Surface Texture"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Consistency"
+                row.placeholder = "description"
+            }
+            <<< TextRow() { row in
+                row.title = "Periodontal Pockets"
+                row.placeholder = "description"
+            }
+            +++ Section("")
+            <<< TextRow() { row in
+                row.title = "Oral hygiene status"
+                row.placeholder = "hygiene status"
+            }
+            <<< SegmentedRow<String>() { row in
+                row.title = "Halitosis"
+                row.options = ["Yes","No"]
+                }.cellSetup { cell, _ in
+                    guard let titleLabel = cell.titleLabel else {return}
+                    cell.segmentedControl.widthAnchor.constraint(equalTo: titleLabel.widthAnchor).isActive = true
+            }
+            <<< SegmentedRow<String>() { row in
+                row.title = "Stains"
+                row.options = ["Yes","No"]
+                }.cellSetup { cell, _ in
+                    guard let titleLabel = cell.titleLabel else {return}
+                    cell.segmentedControl.widthAnchor.constraint(equalTo: titleLabel.widthAnchor).isActive = true
+            }
+            <<< SegmentedRow<String>() { row in
+                row.title = "Calculus"
+                row.options = ["Yes","No"]
+                }.cellSetup { cell, _ in
+                    guard let titleLabel = cell.titleLabel else {return}
+                    cell.segmentedControl.widthAnchor.constraint(equalTo: titleLabel.widthAnchor).isActive = true
+            }
+        
+            +++ Section("Hard tissue examination")
+            +++ Section("Description of specific findings")
+            <<< TextAreaRow() { row in
+                row.placeholder = "Description"
+            }
+            +++ Section("Clinical Diagnosis")
+            <<< TextAreaRow() { row in
+                row.placeholder = "Description"
+            }
+            +++ Section("Differential Diagnosis")
+            <<< TextAreaRow() { row in
+                row.placeholder = "Description"
+            }
+            +++ Section("Investigation and results")
+            <<< TextAreaRow() { row in
+                row.placeholder = "Description"
+            }
+            +++ Section("Final diagnosis")
+            <<< TextAreaRow() { row in
+                row.placeholder = "Description"
+            }
+            +++ Section("Treatment plan")
+            <<< TextAreaRow() { row in
+                row.placeholder = "Description"
+            }
+            +++ Section("Drugs prescribed")
+            <<< TextAreaRow() { row in
+                row.placeholder = "Description"
+            }
+            +++ Section("Examined by")
+            <<< TextRow() { row in
+                row.title = "Student"
+                row.placeholder = "Student name"
+            }
+            <<< TextRow() { row in
+                row.title = "Staff"
+                row.placeholder = "Staff name"
             }
     }
 }
