1ccec11dd84f519e13ec3f04fe266b55e793eaaf
Chaitanya <chaitanya2.1999@gmail.com>
2019-11-09 13:56:12 +0530

    Load existing patient details

--- DentalAssessment/DentalAssessment/departmentOneViewController.swift
+++ DentalAssessment/DentalAssessment/departmentOneViewController.swift
@@ -15,6 +15,7 @@ class departmentOneViewController: UIViewController {
     var ref: DatabaseReference?
     var dbHandle: DatabaseHandle?
     var pid : String = ""
+    var patient = Patient()
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -38,7 +39,38 @@ class departmentOneViewController: UIViewController {
                 if (phone_val == phone){
                     self.pid = (snapshot.ref.key!)
                     print("pid from firebase: \(self.pid)")
-                    self.startAssessmentView()
+                    
+                    self.ref?.child("Patients").observe(.childAdded, with: { (snapshot) in
+                        
+                        if !snapshot.exists() {return}
+                        
+                        if snapshot.key != self.pid{return}
+                        
+                        if let Dictionary = snapshot.value as? [String:AnyObject] ,Dictionary.count > 0{
+
+                            print("inside if let")
+
+                            guard let name = Dictionary["name"] as? String else {return}
+                            print(name)
+                            guard let phone = Dictionary["phone"] as? String else {return}
+                            print(phone)
+                            guard let age = Dictionary["age"] as? Int else {return}
+                            print(age)
+                            guard let sex = Dictionary["sex"] as? Int else {return}
+                            guard let addr = Dictionary["address"] as? String else {return}
+
+                            
+                            self.patient.name = name
+                            self.patient.phone = phone
+                            self.patient.sex = sex
+                            self.patient.age = age
+                            self.patient.address = addr
+                            self.patient.id = snapshot.key
+                            self.startAssessmentView()
+
+                        }
+                    })
+                    
                 }
             }
             else {
@@ -59,7 +91,8 @@ class departmentOneViewController: UIViewController {
     func startAssessmentView(){
         let pavc = UIStoryboard.init(name: "PatientAssessment", bundle: Bundle.main).instantiateInitialViewController() as? CaseSheetViewController
         guard let patientAssessment = pavc else {return}
-        patientAssessment.patient.id = pid
+        patientAssessment.patient = self.patient
+        print(self.patient)
         self.navigationController?.pushViewController(patientAssessment, animated: true)
     }
 }
